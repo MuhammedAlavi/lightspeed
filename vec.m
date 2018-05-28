dd3b8b91193376fbf18cf39f938b3d8c51fd914b
muhammed <muhammed.alavi@yahoo.com>
2018-05-28 16:47:49 +0430

    a command added to vec.m file and it explain the application of the file

--- vec.m
+++ vec.m
@@ -1,3 +1,10 @@
 function v = vec(a)
 
+%VEC, sorts all the elements of 'a' in a column
+% order.
+% Input:
+% 	a[vector or matrix]: input elements
+% Output: 
+% 	v[vector]: elements of a in a column order
+
 v = a(:);
